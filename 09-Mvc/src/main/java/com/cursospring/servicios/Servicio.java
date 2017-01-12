43921b7b530de4fffd9abad59b4a4c09ebb60eb2
ATSTAFF <ATSTAFF>
2017-01-12 14:11:09 +0100

    Mediodia del cuarto dia

---
+++ 09-Mvc/src/main/java/com/cursospring/servicios/Servicio.java
@@ -0,0 +1,20 @@
+package com.cursospring.servicios;
+
+import org.springframework.stereotype.Service;
+
+import com.cursospring.entidades.Cliente;
+
+@Service
+public class Servicio {
+
+	public void altaCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void bajaCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
